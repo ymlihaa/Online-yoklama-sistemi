d13bd2e17bde357bdffe391162a7c5ceaf57c706
Talha Toprak <toprak.urfa@gmail.com>
2020-01-09 00:35:43 +0300

    qr kod backend den alındı. ve gosterildi

--- src/components/qrCode.js
+++ src/components/qrCode.js
@@ -3,27 +3,63 @@ import QRCode from "react-google-qrcode";
 const axios = require("axios");
 
 class QR extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      qr: '',
+      message: '',
+    }
+  }
+  rollcallFinish() {
+    axios.post(
+      "http://ec2-3-15-21-159.us-east-2.compute.amazonaws.com:8080/ymgk-api2/teachers/classrooms/finish/rollcall?classroomId=10", {},
+      { headers: { Authorization: 'Bearer ' + localStorage.getItem("Token") } }
+    ).then(res => {
+      console.log(res);
+
+    }).catch(err => console.log(err))
+
+  }
+  rollcallStart() {
+    
+    this.setState({ message: "Yükleniyor.." });
     const token = localStorage.getItem("Token");
 
     const model = {
       classroomId: 9
     };
 
-    const response = axios.post(
-      "http://ec2-3-15-21-159.us-east-2.compute.amazonaws.com:8080/ymgk-api2/teachers/classrooms/start/rollcall?classroomId=10",
-      {
-        headers: {
-          Authorization: "Bearer " + token
-        }
-      }
-    );
-    console.log(response);
+    axios.post(
+      "http://ec2-3-15-21-159.us-east-2.compute.amazonaws.com:8080/ymgk-api2/teachers/classrooms/start/rollcall?classroomId=10", {},
+      { headers: { Authorization: 'Bearer ' + token } }
+    ).then(res => {
+      console.log(res);
+      let code = res.data.result.qrCode;
+      console.log(code);
+      this.setState({ qr: code });
+    }).catch(error => {
+      console.log(error.response.status)
+      if(error.response.status=400)
+      this.setState({ message: "Bu Yoklama Daha Önce Başlatılmış. Lütfen Yoklama Bittirin.." });
+    })
+
+  }
+  componentDidMount() {
+    this.rollcallStart();
+  }
+  render() {
+
 
     return (
-      <div className="qrCode">
-        <QRCode data="HASAN" size={480} framed />
+      <div className="qrCode text-center">
+        {this.state.qr.length > 0 ? <QRCode data={this.state.qr} size={480} framed /> : <h6>{this.state.message}</h6>}
+
+        <div className="col-md-12 text-center">
+
+          <button className="btn btn-danger" onClick={this.rollcallFinish}>Yoklamayı Bitir</button>
+        </div>
       </div>
+
     );
   }
 }
